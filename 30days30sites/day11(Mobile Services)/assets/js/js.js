01f14eb7934d7c1b9c8bb371ad765383180a91c5
Eissa Ibrahim <33483404+EissaIbrahim@users.noreply.github.com>
2017-12-11 21:13:45 +0300

    Add files via upload

---
+++ 30days30sites/day11(Mobile Services)/assets/js/js.js
@@ -0,0 +1,84 @@
+$(".firstimage").click(function(){
+    $(".popwindow1").dialog({
+      title:"Class G wagen",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+    });
+});
+
+
+$(".secondimage").click(function(){
+  $(".popwindow2").dialog({
+     title:"S class 222",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+  });
+});
+
+$(".thirdimage").click(function(){
+  $(".popwindow3").dialog({
+     title:"Range Rover sport",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+  });
+});
+
+$(".fourthimage").click(function(){
+  $(".popwindow4").dialog({
+     title:"BMW m3",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+  });
+});
+
+$(".fifthimage").click(function(){
+  $(".popwindow5").dialog({
+     title:"Chevrolet Camaro Z11",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+  });
+});
+
+$(".sixthimage").click(function(){
+  $(".popwindow6").dialog({
+     title:"Mitsubishi Pajero",
+      width: 430,
+      height: 250,
+      modal:true,
+      buttons: {
+        Close: function(){
+          $(this).dialog('close');
+        }
+      }
+  });
+});
